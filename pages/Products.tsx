6a63a1f4e08f8679f9770d86321b103110e944ce
mahmoudkhalifakh91-arch <mahmoudkhalifa.kh91@gmail.com>
2026-04-10 13:01:39 +0200

    Delete pages directory

--- pages/Products.tsx
+++
@@ -1,4 +0,0 @@
-
-// This file is deprecated. Use components/ProductTable.tsx and separate warehouse pages.
-import { ProductTable } from '../components/ProductTable';
-export const Products = ProductTable; 
