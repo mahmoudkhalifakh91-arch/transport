325fe961997bfe22fae4e99953521989848902e1
mahmoudkhalifakh91-arch <mahmoudkhalifa.kh91@gmail.com>
2026-04-10 13:03:51 +0200

    Delete DetailedFinishedTable.tsx

--- DetailedFinishedTable.tsx
+++
@@ -1,3 +0,0 @@
-// This file is a duplicate of components/DetailedFinishedTable.tsx and is maintained for compatibility.
-import { DetailedFinishedTable } from './components/DetailedFinishedTable';
-export default DetailedFinishedTable;
